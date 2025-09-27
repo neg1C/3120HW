04a8eddb1452613982fed9e092a40ab34e614799
Percy <percywong20051120@gmail.com>
2025-09-28 01:00:54 +0800

    Changes

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 import { createBoard } from "./board.js";
 
+let name = document.createElement("p");
+  name.textContent = "Hello, I am Wong Yik Hei.";
+
  var boardSize = 10, numberOfMines = boardSize;
 
  createBoard(boardSize, numberOfMines);
